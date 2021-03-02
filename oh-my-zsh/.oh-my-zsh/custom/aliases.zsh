17e158eaddac6f655c75f95cc9bd267a0b323191
Walkersneps <walter@sneps.xyz>
2021-03-02 19:22:05 +0100

    Initial Commit

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+# Command aliases
+alias ff="firefox"
+
+# Git aliases
+alias commit="git commit -S"
+
+# Quick config edits
+alias awrc="nano $DOTFILES_DIR/awesome/.config/awesome/rc.lua"
+
+# xdg-open alias
+alias open="xdg-open"

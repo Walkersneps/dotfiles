4dc4a04e3426a9736f54a87bd8aa33cc4eaa8b0a
Walter <Walkersneps@users.noreply.github.com>
2021-05-01 11:31:41 +0200

    Update aliases.zsh

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,11 +1,39 @@
-# Command aliases
+#### ALT NAMES ####
 alias ff="firefox"
+alias open="xdg-open"
+#############################################
 
-# Git aliases
-alias commit="git commit -S"
 
+### GIT ####
+alias commit='git commit -S' # Faster signed commits
+alias gstat='git status' # Faster git staus
+alias gpush='git push' # Faster pushing
+alias gpull='git pull' # Faster pulling
+unalias glog
+alias glog='git log --decorate --graph --pretty="format:%C(auto)%h %d%Creset | %s | %C(bold)%G?%Creset"' # Great logs
+#############################################
+
+
+#### TMUX ####
+alias t='tmux' # Main tmux shortcut
+alias ta='tmux attach -t' # Faster session attachment
+#############################################
+
+
+#### SWIFT & SPM ####
+# Don't try to correct 'swift build' in 'swift .build'
+alias sb='swift build'
+alias swiftbuild='sb'
+##############################################
+
+
+#### REPLACEMENTS ####
+# Use type instead of which (type is POSIX-standard and works beautifully in recursive stuff, like aliases)
+#alias which='type -a'
+##############################################
+
+
+#### MISC ####
 # Quick config edits
 alias awrc="nano $DOTFILES_DIR/awesome/.config/awesome/rc.lua"
-
-# xdg-open alias
-alias open="xdg-open"
+##############################################

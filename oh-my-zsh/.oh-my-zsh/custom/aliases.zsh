3096166271e493dee46ded539a46f518815cc5f9
Walter <Walkersneps@users.noreply.github.com>
2021-05-01 11:46:41 +0200

    Update aliases.zsh

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -5,10 +5,13 @@ alias open="xdg-open"
 
 
 ### GIT ####
+alias g='git' # Master alias
 alias commit='git commit -S' # Faster signed commits
 alias gstat='git status' # Faster git staus
 alias gpush='git push' # Faster pushing
 alias gpull='git pull' # Faster pulling
+alias gco='git checkout' # Faster checkout
+alias gcheck='git remote -v update' # Fast check if remote server is up-to-date with local
 unalias glog
 alias glog='git log --decorate --graph --pretty="format:%C(auto)%h %d%Creset | %s | %C(bold)%G?%Creset"' # Great logs
 #############################################
